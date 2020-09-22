5edba30c0bd6f2943018b6c5eb10bedc15d01e0d
4Korn <4Kornev@gmail.com>
2020-09-22 16:56:02 +0300

    commit 3

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -4,20 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    //создаем переменные
+    int quantity = 0;
+
+    Spinner spinner;
+
+    ArrayList spinnerArrayList;
+
+    //переменная для заполения спинера
+    ArrayAdapter spinnerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //связываем по id
+        spinner = findViewById(R.id.spinner);
+
+        //Определеяем и добавляем элементы в массив;
+        spinnerArrayList = new ArrayList();
+        spinnerArrayList.add("guitar");
+        spinnerArrayList.add("drums");
+        spinnerArrayList.add("keyboard");
+
+        //передаем в адаптер элементы
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(spinnerAdapter);
     }
 
     //Добавляем количество
     public void increaseQuantity(View view) {
+
+//        quantity = quantity + 1;
+        quantity++;
+
         //связываем по id
         TextView quantityTextView = findViewById(R.id.quantityTextView);
-        quantityTextView.setText("1");
+        quantityTextView.setText("" + quantity);
+    }
+
+    //уменьшаем количество
+    public void decreaseQuantity(View view) {
+        if (quantity > 0) {
+            quantity--;
+
+            //связываем по id
+            TextView quantityTextView = findViewById(R.id.quantityTextView);
+            quantityTextView.setText("" + quantity);
+        }
     }
 }
