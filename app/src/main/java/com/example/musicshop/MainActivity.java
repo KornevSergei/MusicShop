9411dc2e114ecb99e34b5d5ed1ba36dbc8117274
4Korn <4Kornev@gmail.com>
2020-09-28 22:21:25 +0300

    commit 4

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -4,21 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-
-public class MainActivity extends AppCompatActivity {
+//Имплементируем слушатель событий
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     //создаем переменные
     int quantity = 0;
 
     Spinner spinner;
-
     ArrayList spinnerArrayList;
+    HashMap goodsMap;
+    String goodsName;
+    Double price;
+
 
     //переменная для заполения спинера
     ArrayAdapter spinnerAdapter;
@@ -31,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
         //связываем по id
         spinner = findViewById(R.id.spinner);
+        //станавливаем слушатель событий для отображения цены
+        spinner.setOnItemClickListener(this);
+
 
         //Определеяем и добавляем элементы в массив;
         spinnerArrayList = new ArrayList();
@@ -42,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
         spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(spinnerAdapter);
+
+        goodsMap = new HashMap();
+        goodsMap.put("guitar", 500.0);
+        goodsMap.put("drums", 1500.0);
+        goodsMap.put("keyboard", 1000.0);
     }
 
     //Добавляем количество
@@ -53,6 +66,11 @@ public class MainActivity extends AppCompatActivity {
         //связываем по id
         TextView quantityTextView = findViewById(R.id.quantityTextView);
         quantityTextView.setText("" + quantity);
+
+        //Для изменения цены по клику
+        TextView priceTextView = findViewById(R.id.priseTextView);
+        priceTextView.setText("" + quantity * price);
+
     }
 
     //уменьшаем количество
@@ -63,6 +81,21 @@ public class MainActivity extends AppCompatActivity {
             //связываем по id
             TextView quantityTextView = findViewById(R.id.quantityTextView);
             quantityTextView.setText("" + quantity);
+
+            //Для изменения цены по клику
+            TextView priceTextView = findViewById(R.id.priseTextView);
+            priceTextView.setText("" + quantity * price);
+
         }
+    }
+
+
+    //описываем метод отображение цены выбора спинера
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double) goodsMap.get(goodsName);
+        TextView priceTextView = findViewById(R.id.priseTextView);
+        priceTextView.setText("" + quantity * price);
     }
 }
